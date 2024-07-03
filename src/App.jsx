82a9004e905df8af24f81434112d2782f69eb1c0
Alan Olimpio <alancleyton.olimpio@gmail.com>
2024-07-03 14:57:43 -0300

    feat: adiciona props no componente Post

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,82 @@ import * as styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/alanolimpio.png',
+      name: 'Alan Olimpio',
+      role: 'Front end',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é Ignite Feed 🚀',
+      },
+      {
+        type: 'link',
+        link: 'https://github.com/AlanOlimpio/ignitefeed',
+        content: 'jane.design/doctorcare',
+      },
+    ],
+    tags: [
+      {
+        link: 'https://github.com/AlanOlimpio/ignitefeed',
+        content: '#novoprojeto',
+      },
+      {
+        link: 'https://github.com/AlanOlimpio/ignitefeed',
+        content: '#nlw',
+      },
+      {
+        link: 'https://github.com/AlanOlimpio/ignitefeed',
+        content: '#rocketseat',
+      },
+    ],
+    publishedAt: new Date('2024-07-03 12:25:00'),
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link',
+        link: 'https://github.com/AlanOlimpio/ignitefeed',
+        content: 'jane.design/doctorcare',
+      },
+    ],
+    tags: [
+      {
+        link: 'https://github.com/AlanOlimpio/ignitefeed',
+        content: '#novoprojeto',
+      },
+      {
+        link: 'https://github.com/AlanOlimpio/ignitefeed',
+        content: '#nlw',
+      },
+      {
+        link: 'https://github.com/AlanOlimpio/ignitefeed',
+        content: '#rocketseat',
+      },
+    ],
+    publishedAt: new Date('2024-05-03 20:00:00'),
+  },
+  ,
+];
+
 export function App() {
   return (
     <div>
@@ -13,14 +89,17 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Alan Olimpio"
-            content="Lorem ipsum dolor sit consecttur adipisicing elit."
-          />
-          <Post
-            author="Alan Olimpio"
-            content="Lorem ipsum dolor sit consecttur adipisicing elit."
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+                tags={post.tags}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
--- src/componentes/Post/Post.jsx
+++ src/componentes/Post/Post.jsx
@@ -1,39 +1,67 @@
 import React from 'react';
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import Avatar from '../Avatar';
 import Comment from '../Comment';
 import styles from './Post.module.css';
 
-export function Post(props) {
+export function Post({ author, publishedAt, content, tags }) {
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    },
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/alanolimpio.png" alt="Alan Olimpio" />
+          <Avatar src={author.avatarUrl} alt={author.name} />
           <div className={styles.authorInfo}>
-            <strong>{props.author}</strong>
-            <span>{props.content}</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="2 de Maio às 08:13h" dateTime="2024-07-2 08:13:00">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
+        {content.map((item) => {
+          if (item.type === 'paragraph') {
+            return <p>{item.content}</p>;
+          }
+          if (item.type === 'link') {
+            return (
+              <p>
+                👉
+                <a href={item.link} target="_blank">
+                  {item.content}
+                </a>
+              </p>
+            );
+          }
+        })}
+
         <p>
-          Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é Ignite Feed
-          🚀
-        </p>
-        <p>
-          👉<a href="">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="#">#novoprojeto</a>
-          <a href="#">#nlw</a>
-          <a href="#">#rocketseat</a>
+          {tags.map((item) => {
+            return (
+              <a href={item.link} target="_blank">
+                {item.content}
+              </a>
+            );
+          })}
         </p>
       </div>
       <form className={styles.commentForm}>
